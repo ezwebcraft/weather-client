b2acb7baa743796a2aa062bfe6b0ae4385e44dab
Elvin Ramirez <real8686@gmail.com>
2018-06-12 19:57:55 -0400

    feat: set banner for header

---
+++ weather_client.py
@@ -0,0 +1,18 @@
+
+def main()
+    # print header
+    print_the_header()
+    # get zipcode from user
+    # get html from web
+    # parse the html
+    # display for the forcast
+    print("Main section")
+
+def print_the_header():
+    print('--------------------------------------------------')
+    print('                   Weather App                    ')
+    print('--------------------------------------------------')
+    print()
+
+if __name__ == '__main__':
+    main()

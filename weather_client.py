bf08aaa6935a1a5478c89485ad810cc12b12b2e3
Elvin Ramirez <real8686@gmail.com>
2018-06-20 22:59:07 -0400

    add bs4 for html

--- weather_client.py
+++ weather_client.py
@@ -1,5 +1,7 @@
 import requests
 
+import bs4
+
 def main():
     # print header
     print_the_header()
@@ -7,8 +9,9 @@ def main():
     # get zipcode from user
     code = input("Enter Zip code for the weather info you want")
     # get html from web
-    get_html_from_web(code)
+    html = get_html_from_web(code)
     # parse the html
+    get_weather_from_html(html)
     # display for the forcast
     print("Main section")
 
@@ -23,8 +26,12 @@ def print_the_header():
 def get_html_from_web(zipcode):
     url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
     response = requests.get(url)
-    print(response.status_code)
+    #print(response.status_code)
+    return response.text
 
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html)
+    print(soup)
 
 if __name__ == '__main__':
     main()

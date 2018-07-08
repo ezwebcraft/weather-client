8bd3d55f7dc6f0b99360e836c8a1abcf95db0abb
Elvin Ramirez <real8686@gmail.com>
2018-07-08 11:51:05 -0400

    add func cleanup

--- weather_client.py
+++ weather_client.py
@@ -30,13 +30,28 @@ def get_html_from_web(zipcode):
 
 
 def get_weather_from_html(html):
+    
     soup = bs4.BeautifulSoup(html, 'html.parser')
     loc = soup.find(id='location').find('h1').get_text()
-    condition = soup.find(id ='curCond').find( class_ = 'wx-value').get_text()
-    temp = soup.find(id ='curTemp').find( class_ = 'wx-value').get_text()
-    scale = soup.find(id ='curTemp').find( class_ = 'wx-value').get_text()
+    condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+    temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+    scale = soup.find(id='curTemp').find(class_='wx-value').get_text()
+
+    loc = cleanup_text(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
 
     print(condition, temp, scale)
+
+
+def cleanup_text(text: str):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
+
 
 if __name__ == '__main__':
     main()
